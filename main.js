915f78348c1d3aa3ff2e15fba3b39ecb43ac230c
michael dilulo <snoleopard01001101@gmail.com>
2020-10-10 20:16:11 -0700

    Function, mobile, color display right / wrong

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-var magicNumber = Math.floor(Math.random() * 10 + 1);
-var playerGuess = document.getElementById("userGuessInput").value;
+let magicRandomNumber = Math.floor(Math.random() * 10 + 1);
 
+let userGuesses = document.getElementById('previousGuesses');
+let highLowGuess = document.getElementById('highLowGuessNumber');
+let userCheckGuess = document.getElementById('userGuessSubmit');
+let userGuessInput = document.getElementById('userGuessInput');
+let guessCount = 1;
+let previousGuessCount = 0;
+let resetButton;
 
-function submitGuess() {
-    console.log(magicNumber)
-    if (playerGuess == magicNumber) {
-        document.getElementById('userGuessNumber').innerText = "Congrats!"
-    } else if (playerGuess > magicNumber) {
-        document.getElementById('userGuessNumber').innerText = "You guessed too high"
-    } else if (playerGuess < magicNumber) {
-        document.getElementById('userGuessNumber').innerText = "You guessed too low"
+const checkGuess = () => {
+    let userGuess = Number(userGuessInput.value);
+    if (guessCount === 1) {
+        previousGuessCount++;
+    }
+
+    userGuesses.textContent += userGuess + ' ';
+
+    if (userGuess === magicRandomNumber) {
+        highLowGuess.textContent = "You Guessed Right!";
+        highLowGuess.style.backgroundColor = 'green';
+        highLowGuess.style.color = 'white';
+        highLowGuess.style.padding = '10px'
+        highLowGuess.style.borderRadius = "5px"
+    } else {
+        highLowGuess.textContent = "You Guessed Wrong!";
+        highLowGuess.style.backgroundColor = 'red';
+        highLowGuess.style.color = 'white';
+        highLowGuess.style.padding = '10px'
+        highLowGuess.style.borderRadius = "5px"
     }
 }
